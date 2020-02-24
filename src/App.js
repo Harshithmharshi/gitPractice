a73835f809c26a985de7d12e69d3c1dc592e89a2
Harshithmharshi <33544681+Harshithmharshi@users.noreply.github.com>
2020-02-24 14:13:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let a=10;
+  let b=20;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{a+b}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
